6bc2a80cdce3ff8b13891a4fb4a1cdb76f56a241
Liao Yiyang <@>
2017-08-14 11:49:47 +0800

    RankClacService test

---
+++ src/test/java/com/github/eldnine/pagerank/service/RankCalcServiceTest.java
@@ -0,0 +1,21 @@
+package com.github.eldnine.pagerank.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.github.eldnine.pagerank.Application;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class RankCalcServiceTest {
+	@Autowired
+	RankCalcService rankCalcService;
+	
+	@Test
+	public void test() {
+		rankCalcService.run();
+	}
+}
